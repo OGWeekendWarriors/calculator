9a539cef199d8320ae0e42bb561eca93dfb2caf8
Nico <nicolocomendador@yahoo.com>
2020-04-21 12:08:33 -0400

    Added Algebra class and restructured
    I added the algebra classes (Algebra.cpp & Algebra.h) since the previous pull request hasnt been reviewed and cant be merged. In addition to this, I added a new variable that saves the string expression without spaces. Some parsing is faster and better in this case.

---
+++ src/CalcManager/Algebra.cpp
@@ -0,0 +1,175 @@
+/*
+This class was created by Nico Comendador for the purpose of
+WSU CEG 4110-01. This class will handle and house all of the
+functionality for any algebraic operations for a symbolic
+calculator feature add on to the Microsoft Calculator.
+*/
+#include "Algebra.h"
+#include <stack>
+
+using namespace std;
+
+///\return Constructor will instantiate the class variables.
+Algebra::Algebra(string expression)
+{
+    this->expression = format(expression);
+    this->unformattedExpression = expression;
+}
+
+///\return Since there is no garbage collector for C++, we must
+/// include a destructor for the class. Every class with a contructor
+/// must have one.
+Algebra::~Algebra()
+{
+}
+
+///\return This method is used to simplify an expression by
+/// multpliying out the terms to get rid of parenthesis and
+/// exponents.
+string Algebra::simplifyExpression()
+{
+    int result = evaluate(expression);
+    return to_string(result);
+}
+
+///\return This method ensures that the expression has spaces
+/// in between each token. Due to how this class parses and
+/// simplifies things, it is best to first format before
+/// continuing.
+string Algebra::format(string expression)
+{
+    bool space = false;
+    for (int x = 0; x < expression.length(); x++)
+    {
+        if (space)
+        {
+            expression.insert(x, " ");
+            space = false;
+        }
+        else
+        {
+            space = true;
+        }
+    }
+
+    return expression;
+}
+
+///\return This method is responsible for parsing through
+/// and evaluating the expression. Once evaluated, we can
+/// then simplify.
+inline int Algebra::evaluate(string tokens)
+{
+    int i;
+
+    stack<int> values;
+
+    stack<char> ops;
+
+    for (i = 0; i < tokens.length(); i++)
+    {
+        if (tokens[i] == ' ')
+            continue;
+
+        else if (tokens[i] == '(')
+        {
+            ops.push(tokens[i]);
+        }
+
+        else if (isdigit(tokens[i]))
+        {
+            int val = 0;
+
+            while (i < tokens.length() && isdigit(tokens[i]))
+            {
+                val = (val * 10) + (tokens[i] - '0');
+                i++;
+            }
+
+            values.push(val);
+        }
+        else if (tokens[i] == ')')
+        {
+            while (!ops.empty() && ops.top() != '(')
+            {
+                int val2 = values.top();
+                values.pop();
+
+                int val1 = values.top();
+                values.pop();
+
+                char op = ops.top();
+                ops.pop();
+
+                values.push(applyOp(val1, val2, op));
+            }
+
+            if (!ops.empty())
+                ops.pop();
+        }
+        else
+        {
+            while (!ops.empty() && precedence(ops.top()) >= precedence(tokens[i]))
+            {
+                int val2 = values.top();
+                values.pop();
+
+                int val1 = values.top();
+                values.pop();
+
+                char op = ops.top();
+                ops.pop();
+
+                values.push(applyOp(val1, val2, op));
+            }
+
+            ops.push(tokens[i]);
+        }
+    }
+
+    while (!ops.empty())
+    {
+        int val2 = values.top();
+        values.pop();
+
+        int val1 = values.top();
+        values.pop();
+
+        char op = ops.top();
+        ops.pop();
+
+        values.push(applyOp(val1, val2, op));
+    }
+
+    return values.top();
+}
+
+///\return This method applies the operation on the expression
+/// based off of a simple case switch. This is used for
+/// evaluating and so it is needed in order to simplify.
+inline int Algebra::applyOp(int a, int b, char op)
+{
+    switch (op)
+    {
+    case '+':
+        return a + b;
+    case '-':
+        return a - b;
+    case '*':
+        return a * b;
+    case '/':
+        return a / b;
+    }
+}
+
+///\return This method determines the precedence an operator
+/// has. This helps set up the rules (PEMDAS) for evalutaing
+/// and simplifying expressions.
+inline int Algebra::precedence(char op)
+{
+    if (op == '+' || op == '-')
+        return 1;
+    if (op == '*' || op == '/')
+        return 2;
+    return 0;
+}
---
+++ src/CalcManager/Algebra.h
@@ -0,0 +1,40 @@
+/*
+This class was created by Nico Comendador for the purpose of
+WSU CEG 4110-01. This class will handle and house all of the
+functionality for any algebraic operations for a symbolic
+calculator feature add on to the Microsoft Calculator.
+*/
+
+#pragma once
+
+#include <string>
+#include <iostream>
+
+#ifndef _Algebra_
+#define _Algebra_
+
+using namespace std;
+
+class Algebra
+{
+private:
+    ///\return Each Algebra object will hold a
+    /// a string continaing the information of
+    /// the input given from a parent class.
+    string expression;
+    string unformattedExpression;
+
+public:
+    ///\return The following lets the compiler
+    /// know that the following methods will be
+    /// instantiated.
+    inline Algebra(string expression);
+    inline virtual ~Algebra();
+    inline string simplifyExpression();
+    inline int evaluate(string tokens);
+    inline int applyOp(int a, int b, char op);
+    inline int precedence(char op);
+    inline string format(string expression);
+};
+
+#endif
