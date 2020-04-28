ec4c6a655fc86e08833dc3771e018f6e69e84083
Jared-Gibson <jaredscomputer@gmail.com>
2020-04-27 20:21:07 -0400

    Working on createCoeffMatrix()
    -added code for finding LHS using Algebra class (waiting on pull requests)
    -added code for finding RHS using Algebra class (waiting on pull requests)

--- src/CalcManager/CEngine/PartialFrac.cpp
+++ src/CalcManager/CEngine/PartialFrac.cpp
@@ -10,8 +10,8 @@ std::string PartialFrac::PartialFracDecomp(std::string equation)
     std::vector<std::string> fraction = splitEquationString(equation);
     std::string num = fraction[0];
     std::string denom = fraction[1];
-    std::vector<std::vector<PartialFrac::factor>> factoredNum;
-    std::vector<std::vector<PartialFrac::factor>> factoredDenom;
+    std::vector<PartialFrac::factor> factoredNum;
+    std::vector<PartialFrac::factor> factoredDenom;
     factoredNum = factorExpr(num);
     factoredDenom = factorExpr(denom);
     std::vector<std::vector<int>> coeffMatrix = createCoeffMatrix(factoredNum, factoredDenom);
@@ -68,13 +68,57 @@ std::vector<std::string> PartialFrac::splitEquationString(std::string equation)
 //        return false;
 //}
 
-std::vector<std::vector<PartialFrac::factor>> PartialFrac::factorExpr(std::string equation)
+std::vector<PartialFrac::factor> PartialFrac::factorExpr(std::string equation)
 {
     //probably going to use some Algebra class logic here, so I am going to leave this blank for now
 }
 
-std::vector<std::vector<int>> PartialFrac::createCoeffMatrix(std::vector<std::vector<PartialFrac::factor>> num, std::vector<std::vector<PartialFrac::factor>> denom)
+std::vector<std::vector<int>> PartialFrac::createCoeffMatrix(std::vector<PartialFrac::factor> num, std::vector<PartialFrac::factor> denom)
 {
-    
+    //finding the RHS equation
+    //for each position, the value is equal to all other positions multiplied together.
+    std::vector<std::string> RHS;
+    for (int i = 0; i < denom.size(); i++)
+    {
+        std::string expression;
+        for (int j = 0; j < denom.size(); j++)
+        {
+            if (i != j)
+            {
+                std::string tmp = "(" + denom[j].variable + " " + denom[j].operation + " " + std::to_string(denom[j].constant) + ")";
+                if (j == denom.size() - 1)
+                    expression += tmp;
+                else
+                    expression += tmp + " * ";
+            }
+        }
+        //Algebra intermediate = Algebra(expression);
+        //intermediate.format();
+        //RHS[i] = intermediate.simplifyExpression();
+    }
 
+    //finding LHS equation
+    std::vector<std::string> LHS;
+    for (int i = 0; i < num.size(); i++)
+    {
+        std::string expression;
+        for (int j = 0; j < num.size(); j++)
+        {
+            if (i != j)
+            {
+                std::string tmp = "(" + num[j].variable + " " + num[j].operation + " " + std::to_string(num[j].constant) + ")";
+                if (j == num.size() - 1)
+                    expression += tmp;
+                else
+                    expression += tmp + " * ";
+            }
+        }
+        // Algebra intermediate = Algebra(expression);
+        // intermediate.format();
+        // LHS[i] = intermediate.simplifyExpression();
+    }
+    //now that we have the expressions, now we need to add their scalars into the matrix
+    //assuming that the Algebra class has return expressions ordered from highest power to lowest power
+
+    
 }
--- src/CalcManager/CEngine/PartialFrac.h
+++ src/CalcManager/CEngine/PartialFrac.h
@@ -19,14 +19,15 @@ private:
     ///\returns a vector where [0] is the numerator and [1] is denominator
     std::vector<std::string> splitEquationString(std::string equation);
     struct factor {
-        std::string variable;
+        char variable;
+        char operation;
         int constant; };
     ///\returns a factored form of denom using the factor struct to encapsulate a single factor
-    std::vector<std::vector<factor>> factorExpr(std::string equation);
+    std::vector<factor> factorExpr(std::string equation);
     ///\returns a vector that holds a factor at each index
     std::vector<std::string> splitDenom(std::string denom);
     ///\returns a matrix with the coefficients for the partial frac decomp
-    std::vector<std::vector<int>> createCoeffMatrix(std::vector<std::vector<factor>> num, std::vector<std::vector<factor>> denom);
+    std::vector<std::vector<int>> createCoeffMatrix(std::vector<factor> num, std::vector<factor> denom);
     ///\returns the coefficients of the partial fract decomp
     std::vector<int> solveMatrix(std::vector<std::vector<int>> matrix);
     ///\returns a string that represents the partial frac decomp
