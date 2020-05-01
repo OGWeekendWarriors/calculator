b93a3a1f001f7e09820dc19a9ff1e97fb2b96326
Josh Ehlinger <joshehlinger1843@gmail.com>
2020-05-01 15:30:09 -0400

    Add Integral.h header file for Integral class

---
+++ src/CalcManager/Header Files/Integral.h
@@ -0,0 +1,21 @@
+#pragma once
+/*
+ * Module Name: Integral.h
+ * Author: Josh Ehlinger
+ * Module Description:
+ *      The class definition for the Integral class responsible for performing
+ *      basic integration (polynomials, trig functions, ln, a^x, e^x)
+ * Updated 01-May-2020
+ */
+class Integral
+{
+
+    public:
+        Integral();
+        //public functions here
+
+    private:
+
+        //private functions here
+
+};
