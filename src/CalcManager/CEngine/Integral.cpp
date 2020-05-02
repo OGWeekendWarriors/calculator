e3ae0b72c40d49dcf61708f13adbe3233449f069
Josh Ehlinger <joshehlinger1843@gmail.com>
2020-05-02 17:35:53 -0400

    Add functionality for breaking up expression - Add function that finds variable to integrate upon - Start function that splits the expression into coefficients and exponents

--- src/CalcManager/CEngine/Integral.cpp
+++ src/CalcManager/CEngine/Integral.cpp
@@ -14,29 +14,56 @@ std::string Integral::evaluateIntegral(std::string expression)
 {
 
     expression = simplify(expression);
+    //find integratedUpon variable
+    integratedUpon = findIntegratedUpon(expression);
+
     //split coefficients and exponents
-    //make note of variable integating upon
+    splitExpression(expression);
+    
     //evaluate integal for each section partitioned by oeprators
     //build string and add C constant to end of string
-    std::vector<int> coeff/* = ...*/;
-    std::vector<std::string> factors/* = ...*/;
-    std::string finalString = stringBuilder(coeff, factors);
+    
+    std::string finalString = stringBuilder(coeff, exponent);
 
 }
 
 std::string Integral::simplify(std::string expression)
 {
 
-    //Algebra algebraExpression = Algebra(Expression);
-    //algebraExpression.format();
-    //std::string simpExpression = algebraExpression.simplifyExpression();
-    //return simpExpression;
+    /* Algebra Class implementation for later
+    Algebra algebraExpression = Algebra(Expression);
+    algebraExpression.format();
+    std::string simpExpression = algebraExpression.simplifyExpression();
+    return simpExpression;
+    */
 
     //temp return to avoid errors
     return std::string();
 }
 
-std::string Integral::stringBuilder(std::vector<int> coeff, std::vector<std::string> factors)
+std::string Integral::stringBuilder(std::vector<int> coeff, std::vector<int> factors) //add functionality
 {
     return std::string();
+}
+
+char Integral::findIntegratedUpon(std::string expression)
+{
+
+    //find 'dx' form and take just the char after 'd'
+    int pos = expression.find_last_of("d");
+    if (pos != std::string::npos && pos!= expression.length-1)
+    {
+        return expression.at(pos + 1);
+    } else {
+        return 'x';
+    }
+
+}
+
+void Integral::splitExpression(std::string expression)
+{
+
+    //functionality here (run through string, first char is coefficient if not integratedUpon, char after ^ is exponent - if no integratedUpon, exponent is 0)
+    //each coefficient should be paired with an exponent for each term
+
 }
--- src/CalcManager/Header Files/Integral.h
+++ src/CalcManager/Header Files/Integral.h
@@ -20,9 +20,22 @@ class Integral
 
         ///\returns a simplified expression using Algebra class
         std::string simplify(std::string expression);
-        ///\returns ...
-        
+
         ///\returns a string that represents the integrated expression
-        std::string stringBuilder(std::vector<int> coeff, std::vector<std::string> factors);
+        std::string stringBuilder(std::vector<int> coeff, std::vector<int> factors);
+
+        ///Variable to decide what is being integrated upon
+        char integratedUpon;
+
+        ///\returns a char that represetns the variable being integrated on (if no defined, dx assumed)
+        char findIntegratedUpon(std::string expression);
+
+        ///Vector which holds the coefficients of the expression
+        std::vector<int> coeff;
+        ///Vector which holds the exponents tied to each term (2 from x^2 etc.)
+        std::vector<int> exponent;
+
+        ///\returns
+        void splitExpression(std::string expression);
 
 };
