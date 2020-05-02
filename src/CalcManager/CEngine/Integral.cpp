ede4e29494152be92bdeadb2f5fae0a0a8fc1b6e
Josh Ehlinger <joshehlinger1843@gmail.com>
2020-05-01 23:14:19 -0400

    Add planning for implementation and functions - Added planning for implementation and functions - integration of Algebra class simplifyExpression added as comments

--- src/CalcManager/CEngine/Integral.cpp
+++ src/CalcManager/CEngine/Integral.cpp
@@ -5,4 +5,38 @@ Integral::Integral()
 
 }
 
-//Functions here as defined in Integral.h
+Integral::~Integral()
+{
+
+}
+
+std::string Integral::evaluateIntegral(std::string expression)
+{
+
+    expression = simplify(expression);
+    //split coefficients and exponents
+    //make note of variable integating upon
+    //evaluate integal for each section partitioned by oeprators
+    //build string and add C constant to end of string
+    std::vector<int> coeff/* = ...*/;
+    std::vector<std::string> factors/* = ...*/;
+    std::string finalString = stringBuilder(coeff, factors);
+
+}
+
+std::string Integral::simplify(std::string expression)
+{
+
+    //Algebra algebraExpression = Algebra(Expression);
+    //algebraExpression.format();
+    //std::string simpExpression = algebraExpression.simplifyExpression();
+    //return simpExpression;
+
+    //temp return to avoid errors
+    return std::string();
+}
+
+std::string Integral::stringBuilder(std::vector<int> coeff, std::vector<std::string> factors)
+{
+    return std::string();
+}
--- src/CalcManager/Header Files/Integral.h
+++ src/CalcManager/Header Files/Integral.h
@@ -1,5 +1,5 @@
 #pragma once
-/*
+/**
  * Module Name: Integral.h
  * Author: Josh Ehlinger
  * Module Description:
@@ -12,10 +12,17 @@ class Integral
 
     public:
         Integral();
-        //public functions here
+        ~Integral();
+        ///\returns the integral of the expression provided
+        std::string evaluateIntegral(std::string expression);
 
     private:
 
-        //private functions here
+        ///\returns a simplified expression using Algebra class
+        std::string simplify(std::string expression);
+        ///\returns ...
+        
+        ///\returns a string that represents the integrated expression
+        std::string stringBuilder(std::vector<int> coeff, std::vector<std::string> factors);
 
 };
