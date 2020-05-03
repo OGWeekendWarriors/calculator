2aab654055a5be05af5e08e1b639a80e557a4bcd
Jared-Gibson <jaredscomputer@gmail.com>
2020-05-03 17:10:38 -0400

    Removed Team11Unittests project in favor of using CalculatorUnitTests project
    -the plateform for Team11Unittestsproject was incorrect and thus would not allow referencing any of the universal platform projects
    -new direction: create testing files in the Team11 folder within CalculatorUnitTests project

--- src/CalculatorUnitTests/Example.cpp
+++ src/CalculatorUnitTests/Example.cpp
@@ -10,21 +10,23 @@ namespace Example
     /// In order to have VS detect your tests, go to 'Test' -> 'Run All Tests'
     /// After VS has detected your test, you can then go to 'Test' -> 'Test Explorer' to run individual tests
     ///
-    TEST_CLASS(ExampleTestClass){
-        public:
+    TEST_CLASS(ExampleTestClass)
+    {
+    public:
 
-            TEST_METHOD(TestA){
-                //inputs
-                int a = 1;
-                int b = 2;
-                //outputs
-                int expectedOut = 3;
+        TEST_METHOD(TestA)
+        {
+            // inputs
+            int a = 1;
+            int b = 2;
+            // outputs
+            int expectedOut = 3;
 
-                //test code
-                int out = a + b;
+            // test code
+            int out = a + b;
 
-                //Assert statement(s)
-                Assert::AreEqual( out, expectedOut);
-            }
+            // Assert statement(s)
+            Assert::AreEqual(out, expectedOut);
+        }
     };
 }
